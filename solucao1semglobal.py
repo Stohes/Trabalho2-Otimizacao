2ecf9b14b67ce738aee069e39b7fccd873ff65c8
Thiago Claas <t.chies@edu.pucrs.br>
2022-09-21 22:22:50 -0300

    Acho que ta bem perto da solucao 1 sem global

--- solucao1semglobal.py
+++ solucao1semglobal.py
@@ -1,3 +1,6 @@
+from types import NoneType
+
+
 map = [list(line.rsplit()) for line in open("casos de teste/teste2.txt")]
 
 size = int(map[0][0])
@@ -8,23 +11,26 @@ end = map[1][size - 1]
 movY, movX = [-1, -1, 0], [0, 1, 1]
 
 
-def walk(y, x, goldAtual):
+def walk(y, x):
+    gold = 0
+    maiorGold = 0
+
     if not (y < 1 or x > size - 1):  # sair do mapa
-        maiorGold = 0
         casinhaAtual = map[y][x]
 
         if casinhaAtual != "x":
-            novoGold = goldAtual + int(casinhaAtual)
+            
+            for i in range(3):
+                newY = y + movY[i]
+                newX = x + movX[i]
 
-            if y == 1 and x == size - 1:  # final
-                if novoGold > maiorGold:
-                    maiorGold = novoGold
-                    return maiorGold
-            else:
-                for i in range(3):
-                    newY = y + movY[i]
-                    newX = x + movX[i]
-                    walk(newY, newX, novoGold)
-                    return maiorGold
+                novoGold = walk(newY, newX)
+                if novoGold is not None:
+                    gold += novoGold
 
-print(walk(size, 0, 0))
+                    if gold > maiorGold:
+                        maiorGold = gold
+
+            return gold + int(casinhaAtual)
+
+print(walk(size, 0))
