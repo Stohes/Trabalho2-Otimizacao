90a047c062c51bce2cc0be893e1e926b15cd9b96
Stohes <T.Chies@edu.pucrs.br>
2022-09-22 15:31:12 -0300

    Solucao 1 pronta

--- solucao1.py
+++ solucao1.py
@@ -1,33 +1,32 @@
+import time
 map = [list(line.rsplit()) for line in open("casos de teste/teste1.txt")]
 
 size = int(map[0][0])
 start = map[size][0]
 end = map[1][size - 1]
 
-
 movY, movX = [-1, -1, 0], [0, 1, 1]
-maiorGold = 0
 
+def walk(y, x):
+    maxGold = []
+    if y < 1 or x > size - 1:
+        return 0
 
-def walk(y, x, goldAtual):
-    global maiorGold
+    casinhaAtual = map[y][x]
+    if casinhaAtual == "x":
+        return 0
+    
+    if y == 1 and x == size - 1:
+        return int(casinhaAtual)
 
-    if not (y < 1 or x > size - 1):  # sair do mapa
+    for i in range(3):
+        newY = y + movY[i]
+        newX = x + movX[i]
+        gold = int(casinhaAtual) + walk(newY, newX)
+        maxGold.append(gold)
+    return max(maxGold)
 
-        casinhaAtual = map[y][x]
-
-        if casinhaAtual != "x":
-            novoGold = goldAtual + int(casinhaAtual)
-
-            if y == 1 and x == size - 1:  # final
-                if novoGold > maiorGold:
-                    maiorGold = novoGold
-            else:
-                for i in range(3):
-                    newY = y + movY[i]
-                    newX = x + movX[i]
-                    walk(newY, newX, novoGold)
-
-
-walk(size, 0, 0)
-print(maiorGold)
+start = time.time()
+print(walk(size, 0))
+end = time.time()
+print(end - start)
--- solucao1comcaminho.py
+++ solucao1comcaminho.py
@@ -32,6 +32,29 @@ def walk(y, x, goldAtual, caminho):
                     walk(newY, newX, novoGold, caminho + direction[i])
 
 
+# def walk2(y, x):
+#     maxGold = []
+#     path = ""
+#     if y < 1 or x > size - 1:
+#         return 0
+
+#     casinhaAtual = map[y][x]
+#     if casinhaAtual == "x":
+#         return 0
+
+#     if y == 1 and x == size - 1:
+#         return int(casinhaAtual)
+
+#     for i in range(3):
+#         newY = y + movY[i]
+#         newX = x + movX[i]
+#         gold = int(casinhaAtual) + walk2(newY, newX)[0]
+#         maxGold.append(gold)
+#         path += direction[i]
+#     return max(maxGold), path
+
 walk(size, 0, 0, "")
 print(maiorGold)
 print(melhorCaminho)
+
+# print(walk2(size, 0))
