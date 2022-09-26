357baad38cd0366acc86b6e9ecec19bd6c18606c
Stohes <T.Chies@edu.pucrs.br>
2022-09-26 19:12:53 -0300

    Solucao 2 sem caminho

--- solucao2.py
+++ solucao2.py
@@ -1,22 +1,20 @@
-import time
+from cmath import inf
 map = [list(line.rsplit()) for line in open("casos de teste/teste50.txt")]
 
 size = int(map[0][0])
 start = map[size][0]
 end = map[1][size - 1]
 
-movY, movX, direction = [-1, -1, 0], [0, 1, 1], [" N", " NE", " E"]
+movY, movX = [-1, -1, 0], [0, 1, 1]
 
-# caminhos = {}
 casinhasVisitadas = {}
 def walk(y, x):
     maxGold = []
-    # caminho = ""
     if start == "x" or end == "x":
         return "Mapa inválido"
     
     if y < 1 or x > size - 1:
-        return 0
+        return -inf
 
     coordenadasAtuais = f"{y} {x}"
     if coordenadasAtuais in casinhasVisitadas:
@@ -25,7 +23,7 @@ def walk(y, x):
     else:
         casinhaAtual = map[y][x]
         if casinhaAtual == "x":
-            return 0
+            return -inf
 
         if y == 1 and x == size - 1:
             return int(casinhaAtual)
@@ -36,13 +34,7 @@ def walk(y, x):
             gold = int(casinhaAtual) + walk(newY, newX)
             maxGold.append(gold)
             casinhasVisitadas.update({coordenadasAtuais: max(maxGold)})
-            # caminho += direction[i]
-            # caminhos.update({coordenadasAtuais: caminho})
         return max(maxGold)
     
 
-startTime = time.time()
 print(walk(size, 0))
-# print(caminhos.get(f"{size} 0"))
-endTime = time.time()
-print(endTime - startTime)
